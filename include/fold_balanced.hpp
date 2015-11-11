c2e2d6d4c247dfcf548452ecb83e268516ae15df
QuentinDuval <senmenty@gmail.com>
2015-11-11 17:38:26 +0100

    A first version of fold balanced

---
+++ include/fold_balanced.hpp
@@ -0,0 +1,66 @@
+#ifndef FOLD_BALANCED_HPP
+#define FOLD_BALANCED_HPP
+
+
+//-----------------------------------------------------------------------------
+// Version of accumulate with balance (group elements by number of accumulation)
+// @require:
+// - The operation to be associative
+// - The operation to work on two results (elements scanned are results too)
+//-----------------------------------------------------------------------------
+
+template<typename BinaryOp, typename Result>
+struct BalancedAccumulator
+{
+   BalancedAccumulator(BinaryOp op, size_t counter_size)
+      : m_op(op)
+      , m_results(counter_size)
+      , m_marked(counter_size)
+   {}
+   
+   BinaryOp            m_op;
+   std::vector<Result> m_results;
+   std::vector<bool>   m_marked;
+
+   void add(Result res)
+   {
+      for (size_t i = 0; i < m_marked.size(); ++i)
+      {
+         if (!m_marked[i])
+         {
+            m_marked[i] = true;
+            m_results[i] = res;
+            return;
+         }
+
+         res = m_op(m_results[i], res);
+         m_marked[i] = false;
+      }
+   }
+
+   Result finalize(Result res)
+   {
+      for (size_t i = m_marked.size(); i != 0; --i)
+         if (m_marked[i - 1])
+            res = m_op(res, m_results[i - 1]);
+      return res;
+   }
+};
+
+template<typename FwdIterator, typename Result, typename BinaryOp>
+Result fold_balanced(FwdIterator first, FwdIterator last, Result init, BinaryOp op)
+{
+   BalancedAccumulator<BinaryOp, Result> accOp(op, 64);
+   for (; first != last; ++first)
+      accOp.add(*first);
+   return accOp.finalize(init);
+}
+
+template<typename Container, typename Result, typename BinaryOp>
+Result fold_balanced(Container const& container, Result init, BinaryOp op)
+{
+   return fold_balanced(begin(container), end(container), init, op);
+}
+
+
+#endif
