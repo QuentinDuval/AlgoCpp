03d5d2c9bf8b78baccc6348e5c63bc01890734f7
QuentinDuval <senmenty@gmail.com>
2015-11-14 01:44:49 +0100

    Find peak iterative

--- include/find_peak.hpp
+++ include/find_peak.hpp
@@ -39,5 +39,48 @@ typename Container::const_iterator find_peak_rec(Container const& container, Les
    return find_peak_rec(begin(container), end(container), less);
 }
 
+//-----------------------------------------------------------------------------
+
+template <
+   typename RandomAccessIterator,
+   typename Less = std::less<typename std::iterator_traits<RandomAccessIterator>::value_type>
+>
+RandomAccessIterator find_peak(RandomAccessIterator first, RandomAccessIterator last, Less less = Less())
+{
+   using Diff = typename std::iterator_traits<RandomAccessIterator>::difference_type;
+   while (true)
+   {
+      Diff dist = std::distance(first, last);
+      RandomAccessIterator curr = first + dist / 2;
+      if (curr == first)
+         return curr;
+
+      RandomAccessIterator prev = curr - 1;
+      if (less(*curr, *prev))
+      {
+         last = curr;
+         continue;
+      }
+
+      RandomAccessIterator next = curr + 1;
+      if (less(*curr, *next))
+      {
+         first = next;
+         continue;
+      }
+      
+      return curr;
+   }
+}
+
+template<typename Container,
+   typename Less = std::less<typename Container::value_type >>
+   typename Container::const_iterator find_peak(Container const& container, Less less = Less())
+{
+   return find_peak(begin(container), end(container), less);
+}
+
+//-----------------------------------------------------------------------------
+
 
 #endif
