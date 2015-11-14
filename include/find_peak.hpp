37d37f1c40064ddb2783a0cfec5151012fe2e387
QuentinDuval <senmenty@gmail.com>
2015-11-14 02:34:34 +0100

    Preparing list implementation

--- include/find_peak.hpp
+++ include/find_peak.hpp
@@ -18,9 +18,9 @@ using DefLessCont = std::less<typename Container::value_type>;
 // Complexity is O(log(n))
 //-----------------------------------------------------------------------------
 
-template<typename RandomAccessIterator,
-         typename Less = DefLessIter<RandomAccessIterator>>
-RandomAccessIterator find_peak_rec(RandomAccessIterator first, RandomAccessIterator last, Less less = Less())
+template<typename RandomAccessIterator, typename Less>
+auto find_peak_rec_impl(RandomAccessIterator first, RandomAccessIterator last,
+                        Less less, std::random_access_iterator_tag)
 {
    auto curr = first + std::distance(first, last) / 2;
    if (curr == first)
@@ -37,6 +37,19 @@ RandomAccessIterator find_peak_rec(RandomAccessIterator first, RandomAccessItera
    return curr;
 }
 
+template<typename ForwardIterator, typename Less>
+auto find_peak_rec_impl(ForwardIterator first, ForwardIterator last,
+                        Less less, std::bidirectional_iterator_tag)
+{
+   return first;
+}
+
+template<typename Iterator, typename Less = DefLessIter<Iterator>>
+Iterator find_peak_rec(Iterator first, Iterator last, Less less = Less())
+{
+   return find_peak_rec_impl(first, last, less, typename std::iterator_traits<Iterator>::iterator_category());
+}
+
 template<typename Container, typename Less = DefLessCont<Container>>
 auto find_peak_rec(Container const& container, Less less = Less())
 {
@@ -45,9 +58,9 @@ auto find_peak_rec(Container const& container, Less less = Less())
 
 //-----------------------------------------------------------------------------
 
-template<typename RandomAccessIterator,
-         typename Less = DefLessIter<RandomAccessIterator>>
-RandomAccessIterator find_peak(RandomAccessIterator first, RandomAccessIterator last, Less less = Less())
+template<typename RandomAccessIterator, typename Less>
+auto find_peak_impl(RandomAccessIterator first, RandomAccessIterator last,
+                    Less less, std::random_access_iterator_tag)
 {
    while (true)
    {
@@ -61,6 +74,19 @@ RandomAccessIterator find_peak(RandomAccessIterator first, RandomAccessIterator 
       else if (less(*curr, *next))  first = next;
       else                          return curr;
    }
+}
+
+template<typename ForwardIterator, typename Less>
+auto find_peak_impl(ForwardIterator first, ForwardIterator last,
+                    Less less, std::bidirectional_iterator_tag)
+{
+   return first;
+}
+
+template<typename Iterator, typename Less = DefLessIter<Iterator>>
+Iterator find_peak(Iterator first, Iterator last, Less less = Less())
+{
+   return find_peak_impl(first, last, less, typename std::iterator_traits<Iterator>::iterator_category());
 }
 
 template<typename Container, typename Less = DefLessCont<Container>>
